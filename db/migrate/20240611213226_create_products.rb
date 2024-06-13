2a69492ea7140ac85e3606c462d205047ebe6f03
Laurent Curau <laurent@@marketer.tech>
2024-06-13 15:12:14 +0200

    Add product fields

--- db/migrate/20240611213226_create_products.rb
+++ db/migrate/20240611213226_create_products.rb
@@ -1,14 +1,19 @@
 class CreateProducts < ActiveRecord::Migration[7.1]
   def change
+    enable_extension("citext") # case-insensitive text
+
     create_table :products do |t|
-      t.string :sku, index: true, null: false
+      t.citext :sku, index: true, null: false # index SKUs on case-insensitive text
       t.string :name, null: false
       t.text :description
       t.string :colour
       t.integer :pac_size
       t.decimal :price, precision: 10, scale: 2, null: false
       t.integer :width_in_mm
+      t.integer :depth_in_mm
       t.integer :height_in_mm
+      t.integer :diameter_in_mm
+      t.integer :volume_in_ml
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,21 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_11_213226) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.string "sku", null: false
+    t.citext "sku", null: false
     t.string "name", null: false
     t.text "description"
     t.string "colour"
     t.integer "pac_size"
     t.decimal "price", precision: 10, scale: 2, null: false
     t.integer "width_in_mm"
+    t.integer "depth_in_mm"
     t.integer "height_in_mm"
+    t.integer "diameter_in_mm"
+    t.integer "volume_in_ml"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sku"], name: "index_products_on_sku"
