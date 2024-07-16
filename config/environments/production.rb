0a4d1d128797dc01479a49988953be0a4eed8033
Laurent Curau <laurent@@marketer.tech>
2024-07-16 13:43:58 +0200

    Fix order mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "afida_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = Rails.application.credentials.dig(:app_host)
+
+  config.action_mailer.default_url_options = {
+    host: Rails.application.credentials.dig(:app_host)
+  }
 
   config.action_mailer.delivery_method = :mailgun
 
--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
 
+  def new_order_email
+    OrderMailer
+      .with(order: Order.last, user: User.first)
+      .new_order_email
+  end
 end
