4db71329c35120f63edff29f6f1c0669998f7389
Laurent Curau <github@lqro.slmail.com>
2024-09-19 16:04:15 +0200

    Show PAC Size in order confirmation email

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class OrderMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "order confirmation email" do
+    order = orders(:one)
+    user = users(:one)
+    email = OrderMailer.with(order: order, user: user).new_order_email
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ["orders@afida.com"], email.bcc
+    assert_equal [user.email], email.to
+    assert_equal "Your order has been placed!", email.subject
+
+    assert_includes email.body.to_s, "<strong>Item:</strong> Product One"
+    assert_includes email.body.to_s, "<strong>SKU:</strong> p-one"
+    assert_includes email.body.to_s, "<strong>Quantity:</strong> 2"
+    assert_includes email.body.to_s, "<strong>PAC Size:</strong> 1000 pieces"
+    assert_includes email.body.to_s, "<strong>Price:</strong> £11.00"
+    assert_includes email.body.to_s, "<strong>Total:</strong> £22.00"
+  end
 end
