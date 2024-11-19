1384a17ee720ba99442c0c8cdefc6ee28ea38ab9
Laurent Curau <github@lqro.slmail.com>
2024-11-19 21:11:19 +0100

    Upgrade to Kamal 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,12 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  # Skip http-to-https redirect for the default health check endpoint.
+  config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
+  # Skip DNS rebinding protection for the default health check endpoint.
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
 
