76cb47e98cdb8cbb0b17a63bf4094213f41540e0
Laurent Curau <github@lqro.slmail.me>
2025-02-25 16:58:19 +0100

    Sort users by first name

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.all.sort_by(&:first_name)
   end
 
   # GET /users/1 or /users/1.json
