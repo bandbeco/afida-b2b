3ed06b8c7c994f2b782615eee3a420ee8ca1135f
Laurent Curau <github@lqro.slmail.me>
2025-09-09 16:00:00 +0200

    Require posthog-ruby

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'posthog'
+
 $posthog = PostHog::Client.new(
   api_key: 'phc_TOrEF0a6Sd7duFHYpwKfcYqfz44r5K9dvQUR3WQw40H',
   host: 'https://eu.i.posthog.com',
