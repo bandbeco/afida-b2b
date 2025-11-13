2b4a8616eaf7c2e9bdb73a322f6da23de04eb75d
Laurent Curau <github@lqro.slmail.me>
2025-11-13 15:40:12 +0100

    chore: Make importmap audit non-blocking for push

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger]
-  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
-  config.traces_sample_rate = 1.0
+unless Rails.env.test?
+  Sentry.init do |config|
+    config.breadcrumbs_logger = [ :active_support_logger ]
+    config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+    config.traces_sample_rate = 1.0
+  end
 end
